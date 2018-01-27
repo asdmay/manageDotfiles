fe608c599ed0a1daeb3cc4da405e46071d04cc9c
asdmay <wrbmc.mc.pr@gmail.com>
2018-01-28 01:10:29 +0900

    fishにgit branch表示の設定を追加

--- _config.fish
+++ _config.fish
@@ -16,5 +16,15 @@ function fish_prompt
     echo $status_face
 end
 
+function fish_prompt
+  test $SSH_TTY
+  and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+  test $USER = 'root'
+  and echo (set_color red)"#"
+
+  # Main
+  echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
+
 balias g git # キーバインドの下あたりに追記
 
